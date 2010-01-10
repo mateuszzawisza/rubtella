8ae2f15046e38927dd5f900800cb5121dbac363d
Mateusz Zawisza <mateuszzawisza@MacBook-Mateusz-Zawisza.local>
2010-01-10 21:35:01 +0100

    deamonized

--- rubtellad.rb
+++ rubtellad.rb
@@ -1,17 +1,23 @@
 require 'rubtella'
 
 
-Thread.new do
-  server = Rubtella::Listener.new
-  server.listen
-end
+@p = fork do
 
-Thread.new do
-  gnutella = Rubtella::Sender.new Rubtella::Peer.new("70.188.16.73",38093) 
-  gnutella.connect
-  gnutella.send_query("doors")
-end
+  Thread.new do
+    server = Rubtella::Listener.new
+    server.listen
+  end
+  
+  Thread.new do
+    gnutella = Rubtella::Sender.new Rubtella::Peer.new("70.188.16.73",38093) 
+    gnutella.connect
+    gnutella.send_query("doors")
+  end
+  
+  while true
+    sleep 100
+  end
+end 
 
-while true
-  sleep 100
-end
+Process.detach(@p)
+
