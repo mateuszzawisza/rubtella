812cb8894f04fa26b1185d05b8640c0446b9bb5c
Mateusz Zawisza <mateusz.zawisza@gmail.com>
2010-01-11 10:36:01 +0100

    config updated

--- lib/config/config.rb
+++ lib/config/config.rb
@@ -9,8 +9,23 @@ module Rubtella
       guid
     end
 
-    IP_ADDRESS = "127.0.0.1"
-    PORT = 54321
+    if File.exist?(ENV["HOME"] + "/.rubtella/config.rb")
+      load ENV["HOME"] + "/.rubtella/config.rb"
+    else
+      Dir.mkdir(ENV['HOME'] + "/.rubtella") unless File.exist?(ENV['HOME'] + "/.rubtella")
+      File.open(ENV["HOME"] + "/.rubtella/config.rb", 'w') do |f| 
+        f.puts("#put your config here") 
+        f.puts("#") 
+        f.puts("# example below") 
+        f.puts("#") 
+        f.puts("#IP_ADDRESS = 127.0.0.1") 
+        f.puts("#PORT = 54321") 
+      end
+    end
+
+    IP_ADDRESS = "127.0.0.1" unless defined? IP_ADDRESS
+    PORT = 54321 unless defined? PORT
+
     GUID  = generate_guid
 
     
